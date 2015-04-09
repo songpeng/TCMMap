ad77e11ccaac2701dcd9483d9241bb627ba06a20
zusp <zusongpeng@gmail.com>
2015-04-09 21:44:27 +0800

    Update the TCM2sub Script

--- TCM2Substructures_keyexamples.R
+++ TCM2Substructures_keyexamples.R
@@ -5,11 +5,22 @@
 #-- Load package
 require(ChemmineR)
 require(xlsx)
-#-- Load TCM and drugs data.
+
+#-- Load TCM data.
 tcmcid <- read.xlsx("812种化学成分和靶标集合.xlsx",1,colClasses="numeric",
                     colIndex=1)
 
 #-- Function to extract the substructures.
+writecomsub <- function(cids,writefilenm){
+    compounds <- getIds(cids)
+    cid(compounds) <- sdfid(compounds)
+    fpset3 <- fp2bit(compounds)
+    data <- matrix(fpset3@fpma,nrow=length(cids),byrow=FALSE)
+    rownames(data) <- unlist(dimnames(fpset3@fpma)[1])
+    write.table(data,file=writefilenm,quote=FALSE,sep="\t",row.names = TRUE,
+                col.names = FALSE)
+}
+
 compounds <- getIds(unlist(tcmcid$CID))
 cid(compounds) <- sdfid(compounds)
 fpset3 <- fp2bit(compounds)
@@ -18,3 +29,9 @@ fpset3 <- fp2bit(compounds)
 # The matrix is saved as column-wise.
 tcmdata <- matrix(fpset3@fpma,nrow=length(tcmcid$CID),byrow=FALSE)
 rownames(tcmdata) <- unlist(dimnames(fpset3@fpma)[1])
+write.table(tcmdata,file="tcm2subs800_pubchem.txt",quote=FALSE, sep="\t",
+            row.names = TRUE,col.names = FALSE)
+
+#-- Load Drugs data.
+drugcid <- read.table("ANdrugsfromBaiMing.txt",colClasses="numeric",header=FALSE)
+compoundsdrug <- getIds(drugcid$V1)
